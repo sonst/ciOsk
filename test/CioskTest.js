0e9d5b93871f713fabd6e8e965d5086585d78c11
sonst <soeren@vw-dilab.com>
2017-02-03 18:44:29 +0100

    added util tests

--- test/CioskTest.js
+++ test/CioskTest.js
@@ -1,6 +1,11 @@
 
 // run the tests
+
 require('./util/StringBufferTest');
 require('./util/UtilsTest');
+
+
 require('./controller/LayoutPanelTest');
 require('./controller/PanelTest');
+
+require('./controller/PanelSettingsTest');
