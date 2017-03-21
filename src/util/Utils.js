64aa56e0920b41c348bf058aa081bfdbcab6215a
sonst <soeren@vw-dilab.com>
2017-03-21 18:23:29 +0100

    added util fnctns

--- src/util/Utils.js
+++ src/util/Utils.js
@@ -18,4 +18,21 @@ Utils.prototype.printObjectTree = function(object){
   console.log(util.inspect(object,{depth: null}));
 };
 
+Utils.prototype.isEmpty = function(obj) {
+  for(var prop in obj) {
+    if(obj.hasOwnProperty(prop))
+      return false;
+  }
+
+  return JSON.stringify(obj) === JSON.stringify({});
+}
+
+Utils.prototype.getFunctionName = function(fn) {
+  var ret = fn.toString();
+  ret = ret.substr('function '.length);
+  ret = ret.substr(0, ret.indexOf('('));
+  return ret;
+}
+
+
 module.exports = new Utils();
